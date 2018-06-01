c3796205aace4cbf5abc5e75b1157f6c36a33ac2
Chia Khai Fong <shenfeng82@gmail.com>
2018-06-01 11:11:01 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Chia Khai Fong on 1/6/18.
+//  Copyright © 2018 Chia Khai Fong. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
